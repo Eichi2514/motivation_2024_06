b7ad07abab3a8401150b375e5acf99e2ac93200f
Eichi2514 <picon1317@gmail.com>
2024-06-28 10:27:36 +0900

    리스트 추가

--- src/main/java/org/koreait/App.java
+++ src/main/java/org/koreait/App.java
@@ -1,5 +1,7 @@
 package org.koreait;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -11,18 +13,19 @@ public class App {
     }
 
     public void run() {
-        int number = 1;
-        System.out.println("== motivation 실행 ==");
+        int lastid = 1;
+        ArrayList<String> Memo = new ArrayList<>();
+        System.out.println("== motivation execution ==");
 
         while (true) {
-            System.out.printf("명령어) ");
+            System.out.printf("command) ");
             String cmd = sc.nextLine().trim();
 
             if (cmd.equals("exit")) {
-                System.out.println("== motivation 종료 ==");
+                System.out.println("== motivation exit ==");
                 break;
             } else if (cmd.length() == 0) {
-                System.out.println("명령어를 입력해주세요");
+                System.out.println("command를 입력해주세요");
                 continue;
             }
 
@@ -32,37 +35,38 @@ public class App {
                 System.out.printf("source : ");
                 String source = sc.nextLine();
 
-                System.out.println("== " + number + "번 motivation이 등록 되었습니다 ==");
-                number++;
+                Memo.add(Integer.toString(lastid));
+                Memo.add(motivation);
+                Memo.add(source);
+
+
+                System.out.println("== " + lastid + "번 motivation이 등록 되었습니다 ==");
+                lastid++;
+            }
+            if (cmd.equals("list")) {
+                if (Memo.size() == 0) {
+                    System.out.println("등록된 motivation이 없습니다");
+                } else {
+                    System.out.println("id // motivation // source");
+                    for (int i = Memo.size() - 1; i > 0; i -= 3) {
+                        System.out.println(Memo.get(i - 2) + " // " + Memo.get(i - 1) + " // " + Memo.get(i));
+                    }
+                }
+            }
+
+            if (cmd.contains("remove")) {
+                if (cmd.length() > 6) {
+                    int ListIndex = cmd.charAt(cmd.length() - 1);
+                    ListIndex -= 49;
+                    Memo.remove(ListIndex - 0);
+                    Memo.remove(ListIndex + 1);
+                    Memo.remove(ListIndex + 2);
+                    System.out.println((ListIndex + 1) + "번이 삭제 되었습니다");
+                } else {
+                    System.out.println("error : remove뒤에 수정할 번호를 추가해주세요");
+                }
             }
         }
     }
 }
 
-// == 목표 ==
-//명령어) 등록
-//motivation :
-//source :
-//1번 motivation이 등록 되었습니다
-//명령어) add
-//motivation :
-//source :
-//2번 motivation이 등록 되었습니다
-//
-//명령어) 목록
-//== motivation 목록 ==
-//====================
-//번호 / source / motivation
-//  2  /        /
-//  1  /        /
-//
-//명령어) 수정 2
-//motivation :
-//source :
-//2번 motivation이 수정 되었습니다
-//
-//명령어) 삭제
-//1번 motivation이 삭제 되었습니다
-//
-//명령어) system exit
-//== motivation 종료 ==
