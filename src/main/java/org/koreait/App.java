2e276f0212705f03024814e46ea5b89451292e6a
Eichi2514 <picon1317@gmail.com>
2024-06-28 11:24:07 +0900

    source의 글자 수가 많을 경우의 처리

--- src/main/java/org/koreait/App.java
+++ src/main/java/org/koreait/App.java
@@ -13,16 +13,18 @@ public class App {
     }
 
     public void run() {
-        int lastid = 1;
-        ArrayList<String> Memo = new ArrayList<>();
         System.out.println("== motivation execution ==");
 
+        int lastId = 0;
+
+        List<Motivation> motivations = new ArrayList<>();
+
         while (true) {
-            System.out.printf("command) ");
+            System.out.print("command) ");
             String cmd = sc.nextLine().trim();
 
             if (cmd.equals("exit")) {
-                System.out.println("== motivation exit ==");
+                System.out.println("== motivation end ==");
                 break;
             } else if (cmd.length() == 0) {
                 System.out.println("command를 입력해주세요");
@@ -30,43 +32,37 @@ public class App {
             }
 
             if (cmd.equals("add")) {
-                System.out.printf("motivation : ");
-                String motivation = sc.nextLine();
-                System.out.printf("source : ");
+                int id = lastId + 1;
+                System.out.print("body : ");
+                String body = sc.nextLine();
+                System.out.print("source : ");
                 String source = sc.nextLine();
 
-                Memo.add(Integer.toString(lastid));
-                Memo.add(motivation);
-                Memo.add(source);
+                Motivation motivation = new Motivation(id, body, source);
 
+                motivations.add(motivation);
 
-                System.out.println("== " + lastid + "번 motivation이 등록 되었습니다 ==");
-                lastid++;
-            }
-            if (cmd.equals("list")) {
-                if (Memo.size() == 0) {
+                System.out.printf("%d번 motivation이 등록 되었습니다\n", id);
+                lastId++;
+            } else if (cmd.equals("list")) {
+
+                if (motivations.size() == 0) {
                     System.out.println("등록된 motivation이 없습니다");
                 } else {
-                    System.out.println("id // motivation // source");
-                    for (int i = Memo.size() - 1; i > 0; i -= 3) {
-                        System.out.println(Memo.get(i - 2) + " // " + Memo.get(i - 1) + " // " + Memo.get(i));
-                    }
-                }
-            }
+                    System.out.println("============ motivation list ============");
+                    System.out.printf("  id   //     source     //   motivation   \n");
+                    System.out.println("=".repeat(41));
 
-            if (cmd.contains("remove")) {
-                if (cmd.length() > 6) {
-                    int ListIndex = cmd.charAt(cmd.length() - 1);
-                    ListIndex -= 49;
-                    Memo.remove(ListIndex - 0);
-                    Memo.remove(ListIndex + 1);
-                    Memo.remove(ListIndex + 2);
-                    System.out.println((ListIndex + 1) + "번이 삭제 되었습니다");
-                } else {
-                    System.out.println("error : remove뒤에 수정할 번호를 추가해주세요");
+                    for (int i = motivations.size() - 1; i >= 0; i--) {
+                        Motivation motivation = motivations.get(i);
+                        if (motivation.getSource().length() > 7) {
+                            System.out.printf("   %d   //   %s   // %s \n", motivation.getId(), motivation.getSource().substring(0, 6) + "...", motivation.getBody());
+                        } else {
+                            System.out.printf("   %d   //   %s   // %s \n", motivation.getId(), motivation.getSource(), motivation.getBody());
+                        }
+                    }
                 }
             }
         }
     }
 }
-
